5acb9454e2644e76f516ee9f454142fa817d8eb3
Bhuvaneshwar <bhuvaneshwarmarri@gmail.com>
2024-04-24 00:11:27 +0530

    created route screen and related sub component

--- app/src/main/java/com/example/buspassapplication/components/SearchBar.kt
+++ app/src/main/java/com/example/buspassapplication/components/SearchBar.kt
@@ -1,2 +1,80 @@
 package com.example.buspassapplication.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.pm.ShortcutInfoCompat
+import com.example.buspassapplication.ui.theme.DimGray
+import com.example.buspassapplication.ui.theme.White
+
+@Composable
+fun SearchBar(
+    onSearchTextChanged: (String) -> Unit,
+    onSearchClicked: () -> Unit
+) {
+    var searchText by remember { mutableStateOf("") }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(color = Color(0xFFEEFEFD),shape = RoundedCornerShape(16.dp)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(){
+        BasicTextField(
+            value = searchText,
+            onValueChange = {
+                searchText = it
+                onSearchTextChanged(it)
+            },
+            textStyle = TextStyle(color = Color.Black),
+            singleLine = true,
+            modifier = Modifier
+                .padding(end = 8.dp)
+                
+        )
+
+        IconButton(
+            onClick = { onSearchClicked() }
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search"
+            )
+        }
+    }
+    }
+}
+
+@Composable
+fun SearchScreen() {
+    var searchQuery by remember { mutableStateOf("") }
+        SearchBar(
+            onSearchTextChanged = { query ->
+                searchQuery = query
+            },
+            onSearchClicked = {
+
+            }
+        )
+}
+
+@Composable
+@Preview
+fun MyApp() {
+   SearchScreen()
+}
--- app/src/main/java/com/example/buspassapplication/screens/RoutesScreen.kt
+++ app/src/main/java/com/example/buspassapplication/screens/RoutesScreen.kt
@@ -1,2 +1,80 @@
 package com.example.buspassapplication.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.AsyncImagePainter.State.Empty.painter
+import com.example.buspassapplication.R
+import com.example.buspassapplication.Screen
+import com.example.buspassapplication.components.HeadingText
+import com.example.buspassapplication.components.NormalText
+import com.example.buspassapplication.components.OutlinedInputField
+import com.example.buspassapplication.components.PasswordField
+import com.example.buspassapplication.components.PrimaryButton
+import com.example.buspassapplication.components.SearchScreen
+import com.example.buspassapplication.ui.theme.Black
+import com.example.buspassapplication.ui.theme.DarkGray
+import com.example.buspassapplication.ui.theme.NavyBlue
+import com.example.buspassapplication.ui.theme.PoppinsMedium
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+
+@ExperimentalMaterial3Api
+@Composable
+fun RouteScreen(){
+Box(modifier=Modifier.fillMaxSize()) {
+    Image(
+        modifier=Modifier
+            .matchParentSize(),
+        contentScale= ContentScale.FillBounds,
+        painter = painterResource(id = R.drawable.map_image),
+        contentDescription = "Photo"
+    )
+
+        Column {
+            NormalText(
+                modifier = Modifier.padding(top = 40.dp, bottom = 20.dp, start = 20.dp),
+                value = "Route Map",
+                fontSize = 42.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = PoppinsMedium,
+                color = Black
+            )
+            SearchScreen()
+
+    }
+}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun RoutePreview(){
+    RouteScreen()
+}
