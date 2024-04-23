c2a26b9811730d8510f941315467d825e38450fa
Bhuvaneshwar <bhuvaneshwarmarri@gmail.com>
2024-04-23 15:33:17 +0530

    created pass component and related utils

---
+++ app/src/main/java/com/example/buspassapplication/components/CircluarImage.kt
@@ -0,0 +1,31 @@
+package com.example.buspassapplication.components
+
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buspassapplication.R
+
+@Composable
+fun CircleWithImage() {
+    Image(
+        modifier=Modifier
+            .size(115.dp)
+            .clip(CircleShape),
+        painter=painterResource(id=R.drawable.test3),
+        contentDescription = "Photo"
+    )
+}
+
+
+@Composable
+@Preview
+fun PreviewCircular(){
+    CircleWithImage()
+}
