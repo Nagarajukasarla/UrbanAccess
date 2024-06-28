44a5170a1da0a29f6a5cc7b5184e1fa511bb5b6f
Bhuvaneshwar <bhuvaneshwarmarri@gmail.com>
2024-06-29 02:49:44 +0530

    some changes

--- app/src/main/java/com/example/buspassapplication/screens/pastTickets/PastTicketsScreen.kt
+++ app/src/main/java/com/example/buspassapplication/screens/pastTickets/PastTicketsScreen.kt
@@ -10,52 +10,77 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.buspassapplication.R
 import com.example.buspassapplication.components.BackNavigationBar
 import com.example.buspassapplication.components.NormalText
 import com.example.buspassapplication.components.PastTicket
+import com.example.buspassapplication.data.Ticket
 import com.example.buspassapplication.ui.theme.Black
 import com.example.buspassapplication.ui.theme.PoppinsMedium
 import toResponsiveDp
 import toResponsiveSp
+import java.time.LocalDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PastTicketsScreen(
     navController: NavHostController,
-    currentUserId: String?
-){
+    currentUserId: String?,
+    viewModel: PastTicketsViewModel = hiltViewModel()
+) {
+    val ticket1 = Ticket(id = "1", from = "NYC", to = "LA", dateTime = LocalDateTime.now(), img = R.drawable.test4)
+    val ticket2 = Ticket(id = "2", from = "SF", to = "LV", dateTime = LocalDateTime.now(), img = R.drawable.test4)
+    val ticket3 = Ticket(id = "3", from = "SEA", to = "SD", dateTime = LocalDateTime.now(), img = R.drawable.test4)
+
+    // Create a list of Ticket objects
+    val tickets1: List<Ticket>? = listOf(ticket1, ticket2, ticket3)
+    val tickets by viewModel.pastTickets.collectAsState(initial = tickets1)
     Column(
-        modifier = Modifier.padding(bottom = 30.toResponsiveDp()) .verticalScroll(rememberScrollState()),
+        modifier = Modifier.padding(bottom = 30.toResponsiveDp()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         BackNavigationBar(navController = navController)
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 28.toResponsiveDp(), top = 20.toResponsiveDp(), end = 30.toResponsiveDp())
+                .padding(start = 33.toResponsiveDp(), top = 20.toResponsiveDp(), end = 33.toResponsiveDp())
+                .verticalScroll(rememberScrollState())
         ) {
             Column {
                 NormalText(
-                    modifier = Modifier.padding(top = 20.toResponsiveDp(), bottom = 0.toResponsiveDp(), start = 10.toResponsiveDp()),
+                    modifier = Modifier.padding(
+                        top = 20.toResponsiveDp(),
+                        bottom = 0.toResponsiveDp(),
+                        start = 10.toResponsiveDp()
+                    ),
                     value = "My Past Tickets",
                     fontSize = 25.toResponsiveSp(),
                     fontWeight = FontWeight.Bold,
                     fontFamily = PoppinsMedium,
                     color = Black
                 )
-                Spacer(modifier = Modifier.height(20.toResponsiveDp()))
                 Column {
-                    PastTicket(title = "From to To")
-                    Spacer(modifier = Modifier.height(10.toResponsiveDp()))
-                    PastTicket(title = "From to To")
+                    tickets?.forEach { ticket ->
+                            ticket.dateTime?.let {
+                                PastTicket(
+                                    from = ticket.from.toString(),
+                                    to = ticket.to.toString(),
+                                    dateofPurchase = it,
+                                    id = ticket.id.toString(),
+                                    qrImage = ticket.img
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(10.toResponsiveDp()))
+                        }
                 }
             }
         }
@@ -65,5 +90,5 @@ fun PastTicketsScreen(
 @Preview
 @Composable
 fun PastTicketsScreenPreview(){
-    PastTicketsScreen(navController = rememberNavController(), currentUserId = null)
+    PastTicketsScreen(navController = rememberNavController(), currentUserId = "1")
 }
--- app/src/main/java/com/example/buspassapplication/screens/pastTickets/PastTicketsViewModel.kt
+++ app/src/main/java/com/example/buspassapplication/screens/pastTickets/PastTicketsViewModel.kt
@@ -1,12 +1,41 @@
 package com.example.buspassapplication.screens.pastTickets
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.example.buspassapplication.R
+import com.example.buspassapplication.data.Ticket
 import com.example.buspassapplication.models.AppViewModel
+import com.example.buspassapplication.models.service.PastTicketService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class PastTicketsViewModel @Inject constructor(
+    private val pastTicketService: PastTicketService
 ) : AppViewModel(){
+    val ticket1 = Ticket(id = "1", from = "NYC", to = "LA", dateTime = LocalDateTime.now(), img = R.drawable.test4)
+    val ticket2 = Ticket(id = "2", from = "SF", to = "LV", dateTime = LocalDateTime.now(), img = R.drawable.test4)
+    val ticket3 = Ticket(id = "3", from = "SEA", to = "SD", dateTime = LocalDateTime.now(), img = R.drawable.test4)
 
 
+    val tickets1: List<Ticket>? = listOf(ticket1, ticket2, ticket3)
+val pastTickets = MutableStateFlow<List<Ticket>?>(null)
+//    init {
+//        viewModelScope.launch {
+//            fetchTickets("tickets")
+//        }
+//    }
+    suspend fun fetchTickets(userId: String) {
+        try {
+            val fetchedTickets = pastTicketService.fetchAllTickets(userId)
+            pastTickets.value = fetchedTickets
+            Log.d("TransactionHistoryViewModel","Transactions fetched: $fetchedTickets")
+        } catch (e: Exception) {
+            // Handle error appropriately, such as logging or displaying a message to the user
+            Log.d("TransactionHistoryViewModel","Error fetching transactions: ${e.message}")
+        }
+    }
 }
